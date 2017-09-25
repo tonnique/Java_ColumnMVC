d2b02d981c1e841bb42cd7f71b6e0fddfe77b28c
tonnique <tonique@bk.ru>
2017-09-25 12:13:59 +0300

    some changes

--- Columns/src/ColumnsLauncher.java
+++ Columns/src/ColumnsLauncher.java
@@ -2,13 +2,10 @@ import javax.swing.JFrame;
 import View.ColumnsFrame;
 import java.awt.*;
 
+
 public class ColumnsLauncher {
 
 	public static void main(String[] args) {
-
-//		ColumnsFrame frame = new ColumnsFrame();
-//		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-//		frame.setVisible(true);
 
 		EventQueue.invokeLater(new Runnable() {
 			@Override
